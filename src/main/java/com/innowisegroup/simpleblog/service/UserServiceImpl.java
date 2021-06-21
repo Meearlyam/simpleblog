83a23c639f715513491f926bb698f9dd515310b1
Meearlyam <shavela99@gmail.com>
2021-06-21 15:14:22 +0300

    Reconfigure applicaion with Spring Boot: deleted SimpleBlogWebApplicationInitializer and replace WebConfig with Application class, replace corresponding dependencies in gradle.build file.

--- src/main/java/com/innowisegroup/simpleblog/service/UserServiceImpl.java
+++ src/main/java/com/innowisegroup/simpleblog/service/UserServiceImpl.java
@@ -9,6 +9,7 @@ import com.innowisegroup.simpleblog.service.mapping.UserMappingService;
 import com.innowisegroup.simpleblog.service.validation.UserValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Comparator;
 import java.util.List;
@@ -16,6 +17,7 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
