b4db9dc5b7cded1cf86bafcbe8757da4c2d7b4f8
Meearlyam <shavela99@gmail.com>
2021-06-23 13:47:37 +0300

    Configured custom appearance for Swagger UI. Remove spare springfox dependencies (only starter is sufficient). Extract swagger configuration into separate class SwaggerConfiguration. Add swagger annotations to UserRestController with all its endpoints (including request params), and to UserDto.

--- src/main/java/com/innowisegroup/simpleblog/controller/UserRestController.java
+++ src/main/java/com/innowisegroup/simpleblog/controller/UserRestController.java
@@ -1,9 +1,12 @@
 package com.innowisegroup.simpleblog.controller;
 
-import com.innowisegroup.simpleblog.model.UserRole;
 import com.innowisegroup.simpleblog.dto.UserDto;
 import com.innowisegroup.simpleblog.exception.UserValidationException;
+import com.innowisegroup.simpleblog.model.UserRole;
 import com.innowisegroup.simpleblog.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +21,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@Api(description = "Users REST controller")
 public class UserRestController {
 
     private final UserService userService;
@@ -28,17 +32,32 @@ public class UserRestController {
     }
 
     @GetMapping
+    @ApiOperation(
+            value = "Gets all users",
+            notes = "Retrieves all existing users as list",
+            response = UserDto.class,
+            responseContainer = "List"
+    )
     public List<UserDto> getAllUsers() {
         return userService.getAllUsers();
     }
 
     @GetMapping(value = "/{id}")
-    public UserDto getUserById(@PathVariable int id) {
+    @ApiOperation(
+            value = "Finds user by id",
+            notes = "Provide an id to look up specific user",
+            response = UserDto.class
+    )
+    public UserDto getUserById(@ApiParam(required = true) @PathVariable int id) {
         return userService.getUserById(id);
     }
 
     @PostMapping
-    public void createUser(@RequestBody UserDto userDto) throws UserValidationException {
+    @ApiOperation(
+            value = "Creates new user",
+            notes = "Provide valid user data to create new user"
+    )
+    public void createUser(@ApiParam(required = true) @RequestBody UserDto userDto) throws UserValidationException {
         try {
             userService.createUser(userDto);
         }
@@ -48,47 +67,97 @@ public class UserRestController {
     }
 
     @PutMapping("/{id}")
-    public void updateUser(@PathVariable int id, @RequestBody UserDto userDto) {
+    @ApiOperation(
+            value = "Updates the user with given id",
+            notes = "Provide user valid data as request body abd id as request param to update existing user"
+    )
+    public void updateUser(@ApiParam(required = true) @PathVariable int id, @RequestBody UserDto userDto) {
         userService.updateUser(id, userDto);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable int id) {
+    @ApiOperation(
+            value = "Deletes the user with given id",
+            notes = "Provide an id to delete user"
+    )
+    public void deleteUser(@ApiParam(required = true) @PathVariable int id) {
         userService.deleteUser(id);
     }
 
     @GetMapping(value = "/passwords")
+    @ApiOperation(
+            value = "Gets passwords of all users",
+            notes = "Retrieves all users passwords and returns them as a list",
+            response = String.class,
+            responseContainer = "List"
+    )
     public List<String> getUsersPasswords() {
         return userService.getUsersPasswords();
     }
 
     @GetMapping(value = "/sorted/password/get/role/{role}")
-    public List<UserDto> getSortedByPasswordUsersByRole(@PathVariable String role) {
+    @ApiOperation(
+            value = "Gets sorted by password user with specified role",
+            notes = "Provide a role an you get users of the specified role which are sorted alphabetically by password as a list",
+            response = UserDto.class,
+            responseContainer = "List"
+    )
+    public List<UserDto> getSortedByPasswordUsersByRole(@ApiParam(required = true) @PathVariable String role) {
         return userService.getSortedByPasswordUsersByRole(UserRole.valueOfLabel(role));
     }
 
     @GetMapping(value = "/capitalized")
+    @ApiOperation(
+            value = "Gets all users capitalizing their names",
+            notes = "Retrieves all users and returns them as a list after capitalizing their names",
+            response = UserDto.class,
+            responseContainer = "List"
+    )
     public List<UserDto> getUsersWithCapitalizedNames() {
         return userService.getUsersWithCapitalizedNames();
     }
 
     @GetMapping(value = "/ordered/lastname/desc")
+    @ApiOperation(
+            value = "Gets users descending ordered by last name",
+            notes = "Retrieves all users in descending order by their last names and returns them as a list",
+            response = UserDto.class,
+            responseContainer = "List"
+    )
     public List<UserDto> getUsersOrderByLastnameDesc() {
         return userService.getUsersOrderByLastnameDesc();
     }
 
     @GetMapping(value = "/ordered/role")
+    @ApiOperation(
+            value = "Gets all users ordered alphabetically by role",
+            notes = "Retrieves all users ordered alphabetically by role and returns them as a list",
+            response = UserDto.class,
+            responseContainer = "List"
+    )
     public List<UserDto> getUsersOrderByRole() {
         return userService.getUsersOrderByRole();
     }
 
     @GetMapping(value = "/name/{name}")
-    public List<UserDto> getUsersByName(@PathVariable String name) {
+    @ApiOperation(
+            value = "Gets users with the specified name",
+            notes = "Provide a name and you get all users with such name as a list",
+            response = UserDto.class,
+            responseContainer = "List"
+    )
+    public List<UserDto> getUsersByName(@ApiParam(required = true) @PathVariable String name) {
         return userService.findUsersByName(name);
     }
 
     @GetMapping(value = "/lastname/{lastname}")
-    public List<UserDto> getUsersByLastnameOrderByName(@PathVariable String lastname) {
+    @ApiOperation(
+            value = "Gets users with specified last name ordered by name",
+            notes = "Provide a last name and you get users with such last name ordered alphabetically by name as a list",
+            response = UserDto.class,
+            responseContainer = "List"
+    )
+    public List<UserDto> getUsersByLastnameOrderByName(@ApiParam(required = true) @PathVariable String lastname) {
         return userService.findUsersByLastnameOrderByName(lastname);
     }
 }
--- src/main/java/com/innowisegroup/simpleblog/dto/UserDto.java
+++ src/main/java/com/innowisegroup/simpleblog/dto/UserDto.java
@@ -1,15 +1,29 @@
 package com.innowisegroup.simpleblog.dto;
 
 import com.innowisegroup.simpleblog.model.UserRole;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
+@ApiModel(description = "Data transfer object that represents a user")
 public class UserDto {
+    @ApiModelProperty(notes = "The unique id of the user")
     private long id;
+
+    @ApiModelProperty(notes = "The user's name")
     private String name;
+
+    @ApiModelProperty(notes = "The user's last name")
     private String lastname;
+
+    @ApiModelProperty(notes = "The user's email")
     private String email;
+
+    @ApiModelProperty(notes = "The user's password")
     private String password;
+
+    @ApiModelProperty(notes = "The user's role")
     private UserRole role;
 }
